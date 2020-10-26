f8edb4fcea73200d464173d924877e41ceb7704b
Rafael Farias Lima <finha.farias@outlook.com>
2020-10-26 06:46:49 -0300

    add service for get repository info on api (#13)
    * refs #6 add service for get repository info on api
    
    * refs #6 fix eslint error for spreading in props
    
    * refs #6 fix validation for map in array of cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,88 +1,30 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
 import GraphCard from '../../components/GraphCard/GraphCard';
+import Api, { RepositoryStats } from '../../services/api';
 
-const cards = [
-  {
-    frequency: 10,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-  {
-    frequency: 2,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-  {
-    frequency: 2,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-  {
-    frequency: 10,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-  {
-    frequency: 2,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-  {
-    frequency: 2,
-    definitionOSS: 1,
-    popularity: 3,
-    friendly: 2,
-    quality: 2,
-    name: 'teste',
-    owner: 'teste',
-    provider: 'github',
-    language: ['nodejs'],
-  },
-];
+const api = new Api();
 
 const Home: React.FC = () => {
+  const [cards, setCards] = useState<RepositoryStats[]>([]);
+
+  useEffect(() => {
+    api.getAllRepositories().then(response => {
+      setCards(response);
+    });
+  }, []);
+
   return (
     <Container maxWidth="md">
       <Grid container spacing={4}>
-        {cards.map(card => (
-          <Grid item xs={12} sm={6} md={4}>
-            <GraphCard {...card} />
-          </Grid>
-        ))}
+        {cards.length &&
+          cards.map(card => (
+            <Grid item xs={12} sm={6} md={4}>
+              <GraphCard {...card} />
+            </Grid>
+          ))}
       </Grid>
     </Container>
   );
