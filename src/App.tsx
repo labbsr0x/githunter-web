a30407aa0f555434b30fe05c611225f3ae73ecb0
Rafael Farias Lima <finha.farias@outlook.com>
2020-10-27 20:15:37 -0300

    make design for home page (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Routes from './routes';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
-const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
-);
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  content: {
+    flex: 1,
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(3),
+  },
+}));
+
+const App: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+
+      <Container component="main" className={classes.content}>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </Container>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import App from './App';
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {},
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
